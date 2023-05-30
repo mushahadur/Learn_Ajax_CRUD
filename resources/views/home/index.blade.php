606fdb2ea3c008c0176296d42612f72f30e6f244
Mushahadur Rahaman Khan <67184635+mushahadur@users.noreply.github.com>
2023-05-30 22:34:06 +0600

    cdn update to file

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,10 +4,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link href="https://cdn.datatables.net/v/bs5/dt-1.13.4/datatables.min.css" rel="stylesheet"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-  </head>
+    <link href="{{asset('/')}}assets/css/bootstrap.min.css" rel="stylesheet" >
+    <link href="{{asset('/')}}assets/css/datatables.min.css" rel="stylesheet" >
+</head>
   <body>
 
 
@@ -59,7 +59,7 @@
     </div>
   </div>
   {{-- add new employee modal end --}}
-  
+
   {{-- edit employee modal start --}}
   <div class="modal fade" id="editEmployeeModal" tabindex="-1" aria-labelledby="exampleModalLabel"
     data-bs-backdrop="static" aria-hidden="true">
@@ -101,7 +101,7 @@
               <input type="file" name="avatar" class="form-control">
             </div>
             <div class="mt-2" id="avatar">
-  
+
             </div>
           </div>
           <div class="modal-footer">
@@ -113,7 +113,7 @@
     </div>
   </div>
   {{-- edit employee modal end --}}
-  
+
 
     <div class="container">
       <div class="row my-5">
@@ -133,10 +133,11 @@
     </div>
 
 
-      <script src="https://cdn.datatables.net/v/bs5/dt-1.13.4/datatables.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" ></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<script src="{{asset('/')}}assets/js/datatables.min.js"></script>
+<script src="{{asset('/')}}assets/js/jquery.min.js"></script>
+<script src="{{asset('/')}}assets/js/bootstrap.bundle.min.js"></script>
+<script src="{{asset('/')}}assets/js/sweetalert2@11.js"></script>
     <script>
 
 
@@ -178,12 +179,12 @@
                             'Employee Added Successfully!',
                             'success',
                         )
-                        
+
                     }
                     $("#add_empoyee_btn").text('Add Employee');
                     $("#add_employee_form")[0].reset();
                     $("#addEmployeeModal").modal('hide');
-                    
+
                 }
             })
         })
